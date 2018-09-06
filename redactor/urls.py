ba16869c9f756beb9bde28cb280691c7ef654915
port80dev <39792024+port80dev@users.noreply.github.com>
2018-09-06 12:30:53 +0700

    Update urls.py

--- redactor/urls.py
+++ redactor/urls.py
@@ -1,4 +1,4 @@
-from django.conf.urls.defaults import url, patterns
+from django.conf.urls import url, patterns
 
 from redactor.views import redactor_upload
 from redactor.forms import FileForm, ImageForm
